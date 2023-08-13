adb1d1dad8625ef1fd36f7507d66e869f2d0e01c
torhovenkov <vladislav.lnx@gmail.com>
2023-08-13 18:39:24 +0300

    Initial Commit

---
+++ NewsApiApp/NewsApiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApiAppApp.swift
+//  NewsApiApp
+//
+//  Created by Vladyslav Torhovenkov on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
